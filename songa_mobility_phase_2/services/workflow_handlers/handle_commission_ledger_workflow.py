2589522050d523b7c110127d3f06f9fc128b119f
Lucky Tsuma <tsumahmumba@gmail.com>
2026-03-24 13:46:04 +0000

    feat: make webhook call upon commission allocation/deduction

--- songa_mobility_phase_2/services/workflow_handlers/handle_commission_ledger_workflow.py
+++ songa_mobility_phase_2/services/workflow_handlers/handle_commission_ledger_workflow.py
@@ -1,34 +1,67 @@
+import json
+
 import frappe
-from songa_mobility_phase_2.songa_app_integration.utils.utils import get_commission_balance_by_driver
+import requests
+
+from songa_mobility_phase_2.songa_app_integration.utils.utils import (
+	get_commission_balance_by_driver,
+	get_energy_kwh_balance_by_driver,
+	get_rental_days_balance_by_driver,
+)
 
 TRIGGERED_STATES = {"Approved", "Rejected"}
 
+
 def handle_commission_ledger_workflow(doc, method):
-    if not doc.has_value_changed("workflow_state") or doc.workflow_state not in TRIGGERED_STATES:
-        return
+	if not doc.has_value_changed("workflow_state") or doc.workflow_state not in TRIGGERED_STATES:
+		return
 
-    try:
-        return_payload = {
-            "driver_id": doc.driver,
-            "transaction_type": doc.transaction_type,
-            "amount": doc.amount,
-        }
+	try:
+		url = frappe.get_single("Songa Customization Settings").songa_webhook_endpoint
 
-        commission_balance = get_commission_balance_by_driver(driver_id=doc.driver)
-        return_payload["current_commission_balance"] = commission_balance.get("balance", 0)
-            
+		if not url:
+			frappe.log_error(
+				"Songa webhook endpoint not found, please check Songa Customization Settings",
+				"Commission Ledger Workflow",
+			)
+			return
 
-        if doc.transaction_type == "Deduction":
-            # return rental trip balance if deduction was used for rental trip
-            # return KWh balance if deduction was used for EV charging
-            # for now, we will just return the commission balance as we are only implementing commission deductions
-            return_payload["deducted_amount"] = doc.amount
+		# initiate logger
+		frappe.utils.logger.set_log_level("INFO")
+		songa_webhook_logger = frappe.logger("songa_webhook_log", allow_site=True, file_count=20)
 
-        # TODO: Get callback URL from Songa Mobility, add it to settings, and make a POST request to that URL with the payload
-        print(f"\n\n\n\n\n Returned Payload: {return_payload} \n\n\n\n\n")
-        return {"status": "success", "message": return_payload}
-    except Exception as e:
-        frappe.log_error(
-            f"Error processing wallet request approval for {doc.name}: {str(e)}",
-            "Wallet Request Workflow"
-        )
+		return_payload = {
+			"driver_id": doc.driver,
+			"transaction_type": doc.transaction_type,
+			"amount": doc.amount,
+			"commission_ledger": doc.name,
+		}
+
+		headers = {"Content-Type": "application/json"}
+
+		commission_balance = get_commission_balance_by_driver(driver_id=doc.driver)
+		return_payload["commission_balance"] = commission_balance.get("balance", 0)
+
+		if doc.transaction_type == "Deduction":
+			usage = doc.usage
+			return_payload["usage"] = usage
+
+			if usage == "Energy recharge":
+				return_payload["energy_kwh_balance"] = get_energy_kwh_balance_by_driver(driver_id=doc.driver)
+			elif usage == "Rental days recharge":
+				return_payload["rental_days_balance"] = get_rental_days_balance_by_driver(
+					driver_id=doc.driver
+				)
+
+			return_payload["deducted_amount"] = doc.amount
+
+		data = json.dumps(return_payload)
+		response = requests.post(url, data=data, headers=headers, verify=True)
+		if response.status_code != 200:
+			frappe.log_error(f"{response}", "Error sending data to webhook endpoint")
+			return
+		songa_webhook_logger.info(f"Driver Commission Ledger: {doc.name}. Response: {response}\n")
+	except Exception as e:
+		frappe.log_error(
+			f"Error processing Commission Ledger for {doc.name}: {e!s}", "Commission Ledger Workflow"
+		)
