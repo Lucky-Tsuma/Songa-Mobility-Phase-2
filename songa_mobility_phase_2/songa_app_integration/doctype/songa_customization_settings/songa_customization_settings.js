6e5926865ec363d5df4b467718572965213a2ea9
Lucky Tsuma <tsumahmumba@gmail.com>
2026-03-30 04:04:53 +0000

    fix: update account filter

--- songa_mobility_phase_2/songa_app_integration/doctype/songa_customization_settings/songa_customization_settings.js
+++ songa_mobility_phase_2/songa_app_integration/doctype/songa_customization_settings/songa_customization_settings.js
@@ -3,20 +3,12 @@
 
 frappe.ui.form.on("Songa Customization Settings", {
 	onload(frm) {
-        frm.set_query("expense_account", () => {
-            return {
-                filters: {
-                    "root_type": "Expense",
-                }
-            }
-        })
-
-        frm.set_query("liability_account", () => {
-            return {
-                filters: {
-                    "root_type": "Liability",
-                }
-            }
-        })
+		frm.set_query("driver_commission_account", () => {
+			return {
+				filters: {
+					root_type: "Expense",
+				},
+			};
+		});
 	},
 });
