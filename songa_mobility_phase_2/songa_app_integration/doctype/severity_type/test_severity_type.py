0244ee2e41549b6a2a3d707f7b39c698ae623842
Lucky Tsuma <tsumahmumba@gmail.com>
2026-04-02 16:47:20 +0300

    feat: add `severity_type` doctype'

---
+++ songa_mobility_phase_2/songa_app_integration/doctype/severity_type/test_severity_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Lucky Tsuma and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSeverityType(FrappeTestCase):
+	pass
