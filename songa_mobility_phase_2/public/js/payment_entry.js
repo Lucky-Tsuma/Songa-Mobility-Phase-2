be388c955674e6dc1aee63a0f2e20efd13014969
Lucky Tsuma <tsumahmumba@gmail.com>
2026-04-14 15:56:55 +0300

    feat: persist `branch` and ``cost_center` values once selected

--- songa_mobility_phase_2/public/js/payment_entry.js
+++ songa_mobility_phase_2/public/js/payment_entry.js
@@ -1,24 +1,61 @@
+// Cache to hold the customer-derived values
+const _paymentEntrycommissionDefaults = {
+    branch: null,
+    cost_center: null,
+};
+
+const clear_branch_and_cost_center = (frm) => {
+    _paymentEntrycommissionDefaults.branch = null;
+    _paymentEntrycommissionDefaults.cost_center = null;
+    frm.set_value("branch", "");
+    frm.set_value("cost_center", "");
+};
+
 frappe.ui.form.on("Payment Entry", {
     party: function (frm) {
         if (frm.doc.party_type === "Supplier") {
             set_payment_entry_branch_and_cost_center(frm);
         }
-    }
+    },
+    // Guard: restore if something else clears or overwrites these fields
+    branch: function (frm) {
+        if (_paymentEntrycommissionDefaults.branch && frm.doc.branch !== _paymentEntrycommissionDefaults.branch) {
+            frm.set_value("branch", _paymentEntrycommissionDefaults.branch);
+        }
+    },
+    cost_center: function (frm) {
+        if (_paymentEntrycommissionDefaults.cost_center && frm.doc.cost_center !== _paymentEntrycommissionDefaults.cost_center) {
+            frm.set_value("cost_center", _paymentEntrycommissionDefaults.cost_center);
+        }
+    },
+    validate: function (frm) {
+        if (_paymentEntrycommissionDefaults.branch && frm.doc.branch !== _paymentEntrycommissionDefaults.branch) {
+            frm.set_value("branch", _paymentEntrycommissionDefaults.branch);
+        }
+
+        if (_paymentEntrycommissionDefaults.cost_center && frm.doc.cost_center !== _paymentEntrycommissionDefaults.cost_center) {
+            frm.set_value("cost_center", _paymentEntrycommissionDefaults.cost_center);
+        }
+    },
 });
 
 const set_payment_entry_branch_and_cost_center = (frm) => {
-    if (!frm.doc.party) return;
+    if (!frm.doc.party) return clear_branch_and_cost_center(frm);
 
     return frappe.call({
         method: "songa_mobility_phase_2.songa_app_integration.utils.utils.get_branch_and_cost_center_by_supplier",
         args: { supplier: frm.doc.party },
         callback: function (r) {
             if (r.message) {
-                supplier_doc = r.message;
-                frm.set_value("branch", supplier_doc.branch);
-                frm.set_value("cost_center", supplier_doc.cost_center);
+                let supplier_doc = r.message;
+
+                _paymentEntrycommissionDefaults.branch = supplier_doc.branch || null;
+                _paymentEntrycommissionDefaults.cost_center = supplier_doc.cost_center || null;
+
+                frm.set_value("branch", supplier_doc.branch || "");
+                frm.set_value("cost_center", supplier_doc.cost_center || "");
             } else {
-                return;
+                return clear_branch_and_cost_center(frm);
             }
         },
         error: function () {
--- songa_mobility_phase_2/public/js/sales_invoice.js
+++ songa_mobility_phase_2/public/js/sales_invoice.js
@@ -1,30 +1,70 @@
+// Cache to hold the customer-derived values
+const _salesInvoicecommissionDefaults = {
+    branch: null,
+    cost_center: null,
+};
+
+const clear_branch_and_cost_center = (frm) => {
+    _salesInvoicecommissionDefaults.branch = null;
+    _salesInvoicecommissionDefaults.cost_center = null;
+    frm.set_value("branch", "");
+    frm.set_value("cost_center", "");
+};
+
 frappe.ui.form.on("Sales Invoice", {
     customer: function (frm) {
+        _salesInvoicecommissionDefaults.branch = null;
+        _salesInvoicecommissionDefaults.cost_center = null;
         set_sales_invoice_branch_and_cost_center(frm);
-    }
+    },
+
+    // Guard: restore if something else clears or overwrites these fields
+    branch: function (frm) {
+        if (_salesInvoicecommissionDefaults.branch && frm.doc.branch !== _salesInvoicecommissionDefaults.branch) {
+            frm.set_value("branch", _salesInvoicecommissionDefaults.branch);
+        }
+    },
+    cost_center: function (frm) {
+        if (_salesInvoicecommissionDefaults.cost_center && frm.doc.cost_center !== _salesInvoicecommissionDefaults.cost_center) {
+            frm.set_value("cost_center", _salesInvoicecommissionDefaults.cost_center);
+        }
+    },
+    validate: function (frm) {
+        if (_salesInvoicecommissionDefaults.branch && frm.doc.branch !== _salesInvoicecommissionDefaults.branch) {
+            frm.set_value("branch", _salesInvoicecommissionDefaults.branch);
+        }
+
+        if (_salesInvoicecommissionDefaults.cost_center && frm.doc.cost_center !== _salesInvoicecommissionDefaults.cost_center) {
+            frm.set_value("cost_center", _salesInvoicecommissionDefaults.cost_center);
+        }
+    },
 });
 
 const set_sales_invoice_branch_and_cost_center = (frm) => {
-    if (!frm.doc.customer) return;
+    if (!frm.doc.customer) return clear_branch_and_cost_center(frm);
+
     return frappe.call({
         method: "songa_mobility_phase_2.songa_app_integration.utils.utils.get_linked_supplier",
         args: { customer: frm.doc.customer },
         callback: function (r) {
-            if (r.message) {
-                return frappe.call({
-                    method: "songa_mobility_phase_2.songa_app_integration.utils.utils.get_branch_and_cost_center_by_supplier",
-                    args: { supplier: r.message },
-                    callback: function (res) {
-                        if (res.message) {
-                            frm.set_value("branch", res.message.branch);
-                            frm.set_value("cost_center", res.message.cost_center);
-                        }
-                    },
-                    error: function () {
-                        frappe.msgprint("An error occurred while retrieving supplier details.");
-                    }
-                });
-            }
+            if (!r.message) return clear_branch_and_cost_center(frm);
+
+            return frappe.call({
+                method: "songa_mobility_phase_2.songa_app_integration.utils.utils.get_branch_and_cost_center_by_supplier",
+                args: { supplier: r.message },
+                callback: function (res) {
+                    if (!res.message) return clear_branch_and_cost_center(frm);
+
+                    _salesInvoicecommissionDefaults.branch = res.message.branch || null;
+                    _salesInvoicecommissionDefaults.cost_center = res.message.cost_center || null;
+
+                    frm.set_value("branch", res.message.branch || "");
+                    frm.set_value("cost_center", res.message.cost_center || "");
+                },
+                error: function () {
+                    frappe.msgprint("An error occurred while retrieving supplier details.");
+                }
+            });
         },
         error: function () {
             frappe.msgprint("An error occurred while retrieving linked supplier.");
