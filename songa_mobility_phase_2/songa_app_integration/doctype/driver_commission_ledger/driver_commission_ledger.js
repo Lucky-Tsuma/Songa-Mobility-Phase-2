736aeaf9caf7c88f401dd7980ca50785b89885a4
Lucky Tsuma <tsumahmumba@gmail.com>
2026-07-01 14:58:13 +0300

    chore: remove unused function

--- songa_mobility_phase_2/songa_app_integration/doctype/driver_commission_ledger/driver_commission_ledger.js
+++ songa_mobility_phase_2/songa_app_integration/doctype/driver_commission_ledger/driver_commission_ledger.js
@@ -1,10 +1,7 @@
 frappe.ui.form.on("Driver Commission Ledger", {
     before_workflow_action(frm) {
-        if (frm.selected_workflow_action === "Approve") {
-
-            if (frm.doc.transaction_type === "Allocation") {
-                return allocateCommission(frm);
-            }
+        if (frm.selected_workflow_action === "Approve" && frm.doc.transaction_type === "Allocation") {
+            return allocateCommission(frm);
         }
     },
 });
@@ -28,35 +25,6 @@ const allocateCommission = (frm) => {
             },
             error: function() {
                 frappe.msgprint("An error occurred while allocating commission.");
-                frappe.dom.unfreeze();
-                reject();
-            }
-        });
-    });
-}
-
-// TODO: Remove unused function
-const deductCommission = (frm) => {
-    return new Promise((resolve, reject) => {
-        frappe.call({
-            method: "songa_mobility_phase_2.songa_app_integration.utils.utils.deduct_commission",
-            args: {
-                wallet_request_name: frm.doc.name
-            },
-            callback: function(r) {
-                if (r.message && r.message.status === "success") {
-                    resolve();
-                    setTimeout(() => {
-                        frappe.msgprint("Commission deducted successfully, new trip created.");
-                    }, 1000); // wait for frappe alert to close before showing success message
-                } else {
-                    frappe.msgprint(r.message);
-                    frappe.dom.unfreeze();
-                    reject();
-                }
-            },
-            error: function() {
-                frappe.msgprint("An error occurred while deducting commission.");
                 frappe.dom.unfreeze();
                 reject();
             }
